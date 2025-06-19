5a8f587e1ecd51230a3515cdedd7196c1dbf6e57
natthida <natthidarungrueang@gmail.com>
2025-06-19 16:47:34 +0700

    add validation test for quest model

--- spec/models/quest_spec.rb
+++ spec/models/quest_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Quest, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "is valid with a title" do
+    quest = Quest.new(title: "Learn Rails", done: false)
+    expect(quest).to be_valid
+  end
+
+  it "is invalid without a title" do
+    quest = Quest.new(title: nil, done: false)
+    expect(quest).to_not be_valid
+  end
 end
