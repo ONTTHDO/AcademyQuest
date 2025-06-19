c4ef2352d7534cf869040247448086b6dcbb8773
natthida <natthidarungrueang@gmail.com>
2025-06-19 13:49:56 +0700

    change root path and add name route for brag

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   resources :quests
   get "up" => "rails/health#show", as: :rails_health_check
 
-  root "posts#index"
-  get "brags/index"
+  root "quests#index"
+  get "/brag", to: "brags#index", as: :brag
 end
